7b84a4ca94d10042d19d62916c8a5bcebddc37fb
shadichy <hotrobb360web@gmail.com>
2022-08-08 10:17:12 +0700

    update readme

--- git-v1.sh
+++ git-v1.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-#set -e
-##################################################################################################################
-# Author 	: Erik Dubois
-# Website   : https://www.erikdubois.be
-# Website	: https://www.arcolinux.info
-# Website	: https://www.arcolinux.com
-# Website	: https://www.arcolinuxd.com
-# Website	: https://www.arcolinuxb.com
-# Website	: https://www.arcolinuxiso.com
-# Website	: https://www.arcolinuxforum.com
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
-##################################################################################################################
 
 # Creating the databases
 cd x86_64
--- setup-git-v5.sh
+++ setup-git-v5.sh
@@ -26,18 +26,18 @@
 
 project=$(basename `pwd`)
 echo "-----------------------------------------------------------------------------"
-echo "this is project https://github.com/erikdubois/"$project
+echo "this is project https://github.com/shadichy/"$project
 echo "-----------------------------------------------------------------------------"
 git config --global pull.rebase false
-git config --global user.name "Erik Dubois"
-git config --global user.email "erik.dubois@gmail.com"
+git config --global user.name "Shadichy"
+git config --global user.email "hotrobb360web@gmail.com"
 sudo git config --system core.editor nano
 #git config --global credential.helper cache
 #git config --global credential.helper 'cache --timeout=32000'
 git config --global push.default simple
 git config --global init.defaultBranch main
 
-git remote set-url origin git@github.com-edu:erikdubois/$project
+git remote set-url origin git@github.com:shadichy/$project
 
 echo "Everything set"
 
